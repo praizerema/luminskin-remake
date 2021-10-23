b3a7fb6b087137032e235137cb25d08247879147
Praise God <49660294+praizerema@users.noreply.github.com>
2021-10-23 18:49:45 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import ExchangeRatePage from "./ExchangeRatePage"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExchangeRatePage/>
     </div>
   );
 }
 
 export default App;
+
+
+// import React from "react";
+// import "./App.css";
+// import { client } from "./ApolloClient/client";
+// import { ApolloProvider,gql} from '@apollo/client';
+// import ExchangeRatesPage from './ExchangeRatePage';
+
+
+// function App() {
+//   client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+//   return (
+//     <ApolloProvider client={client}>
+//       <div className="App">
+//         <ExchangeRatesPage />
+//       </div>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,63 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+// import {client} from "./ApolloClient/client"
+import { RestLink } from "apollo-link-rest";
+// import { HttpLink } from "apollo-link-http";
+// const httpLink = new HttpLink({
+//   uri: "https://48p1r2roz4.sse.codesandbox.io",
+// });
+
+ const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://pangaea-interviews.now.sh/api/graphql",
+});
+client
+  .query({
+    query: gql`
+      query GetProducts {
+        products {
+          id
+          title
+          image_url
+price(
+  currency: USD
+  )
+product_options{
+    title
+prefix
+suffix
+options{
+    id
+    value
+}
+  
+}
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+  client
+  .query({
+    query: gql`
+    query GetCurrency {
+      currency
+    }
+    `}).then(result => console.log(result));
+  
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+  <App />
+</ApolloProvider>,
   document.getElementById('root')
 );
 
